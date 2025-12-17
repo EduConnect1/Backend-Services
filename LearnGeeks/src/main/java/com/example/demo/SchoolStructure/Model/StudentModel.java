35c831be0fa7c6db7ff68ec8b5571244615921e0
Kayinamura-Karimba-Geofrey <geofreykayin@gmail.com>
2025-12-17 21:20:58 +0200

    first

--- LearnGeeks/src/main/java/com/example/demo/SchoolStructure/Model/StudentModel.java
+++ LearnGeeks/src/main/java/com/example/demo/SchoolStructure/Model/StudentModel.java
@@ -1,5 +1,32 @@
 package com.example.demo.SchoolStructure.Model;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "students")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 
 public class StudentModel {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(unique = true, nullable = false)
+    private String admissionNumber;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "class_id", nullable = false)
+    private ClassModel ClassModel;
+
 }
+
+    
+
