6ac795b1d3ac028118049f58180c2530601c9339
Kayinamura-Karimba-Geofrey <geofreykayin@gmail.com>
2025-12-20 20:14:37 +0200

    first

--- LearnGeeks/src/main/java/com/example/demo/SchoolStructure/service/SubjectService.java
+++ LearnGeeks/src/main/java/com/example/demo/SchoolStructure/service/SubjectService.java
@@ -1,5 +1,16 @@
 package com.example.demo.SchoolStructure.service;
+import com.example.demo.SchoolStructure.Model.SubjectModel;
 
-public class SubjectService {
-    
+
+import java.util.List;
+
+public interface SubjectService {
+
+    Subject createSubject(Long classId, Subject subject);
+
+    Subject getSubjectById(Long id);
+
+    List<Subject> getSubjectsByClass(Long classId);
+
+    void deleteSubject(Long id);
 }
