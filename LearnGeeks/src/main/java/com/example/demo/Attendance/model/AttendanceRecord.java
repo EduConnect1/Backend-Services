06c4c18f0d2962eed09d9311c191cb2f07ac101f
Kayinamura-Karimba-Geofrey <geofreykayin@gmail.com>
2025-12-21 00:51:49 +0200

    intendance

--- LearnGeeks/src/main/java/com/example/demo/Attendance/model/AttendanceRecord.java
+++ LearnGeeks/src/main/java/com/example/demo/Attendance/model/AttendanceRecord.java
@@ -1,5 +1,34 @@
 package com.example.demo.Attendance.model;
+import com.example.demo.SchoolStructure.Model.Student;
+import jakarta.persistence.*;
+import lombok.Data;
+@Data
+@Entity
+@Table(name = "attendance_records")
+
 
 public class AttendanceRecord {
     
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "attendance_session_id")
+    private AttendanceSession attendanceSession;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "student_id")
+    private Student student;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private AttendanceStatus status;
+
+    @Column(length = 255)
+    private String remarks;
 }
+
+    
+
