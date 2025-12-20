2e810a586a5793bae76ff0a864278ea7a1b48021
Kayinamura-Karimba-Geofrey <geofreykayin@gmail.com>
2025-12-21 01:40:12 +0200

    intendance

---
+++ LearnGeeks/src/main/java/com/example/demo/Attendance/DTO/ClassAttendanceSummaryResponse.java
@@ -0,0 +1,9 @@
+package com.example.demo.Attendance.DTO;
+
+public class ClassAttendanceSummaryResponse {
+
+        Long classId;
+        String className;
+        long totalStudents;
+        double averageAttendancePercentage; 
+}
---
+++ LearnGeeks/src/main/java/com/example/demo/Attendance/DTO/StudentAttendanceSummaryResponse.java
@@ -0,0 +1,12 @@
+package com.example.demo.Attendance.DTO;
+
+public class StudentAttendanceSummaryResponse {
+    
+        Long studentId;
+        String studentName;
+        long presentCount;
+        long absentCount;
+        long lateCount;
+        double attendancePercentage;
+    
+}
