eb82ab5be6882125fe28ccafb68c310c11c8dcf6
Kayinamura-Karimba-Geofrey <geofreykayin@gmail.com>
2025-12-20 19:50:05 +0200

    first

--- LearnGeeks/src/main/java/com/example/demo/SchoolStructure/service/TeacherService.java
+++ LearnGeeks/src/main/java/com/example/demo/SchoolStructure/service/TeacherService.java
@@ -1,5 +1,19 @@
 package com.example.demo.SchoolStructure.service;
+import com.example.demo.SchoolStructure.model.Teacher;
+import java.util.Set;
+
 
 public class TeacherService {
     
+
+    Teacher createTeacher(Teacher teacher);
+
+    Teacher assignSubjects(Long teacherId, Set<Long> subjectIds);
+
+    Teacher getTeacherById(Long id);
+
+    void deleteTeacher(Long id);
 }
+
+    
+
