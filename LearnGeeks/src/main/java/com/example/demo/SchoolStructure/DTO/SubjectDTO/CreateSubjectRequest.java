36c8a9abbe97c5585510665f236e14c29640a409
Kayinamura-Karimba-Geofrey <geofreykayin@gmail.com>
2025-12-20 22:27:29 +0200

    first

--- LearnGeeks/src/main/java/com/example/demo/SchoolStructure/DTO/SubjectDTO/CreateSubjectRequest.java
+++ LearnGeeks/src/main/java/com/example/demo/SchoolStructure/DTO/SubjectDTO/CreateSubjectRequest.java
@@ -1,5 +1,18 @@
 package com.example.demo.SchoolStructure.DTO.SubjectDTO;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
 public class CreateSubjectRequest {
     
+    @NotBlank(message = "Subject name is required")
+    private String name;
+
+    @NotNull(message = "Class ID is required")
+    private Long classId;
 }
+
+    
+
