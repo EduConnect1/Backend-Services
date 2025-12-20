3d1929555b176e10900016487951695a177cad62
Kayinamura-Karimba-Geofrey <geofreykayin@gmail.com>
2025-12-20 22:37:54 +0200

    first

--- LearnGeeks/src/main/java/com/example/demo/SchoolStructure/DTO/StudentDTO/StudentResponse.java
+++ LearnGeeks/src/main/java/com/example/demo/SchoolStructure/DTO/StudentDTO/StudentResponse.java
@@ -1,5 +1,17 @@
 package com.example.demo.SchoolStructure.DTO.StudentDTO;
+import lombok.Data;
+@Data
 
 public class StudentResponse {
     
+
+
+    private Long id;
+    private String admissionNumber;
+    private Long userId;
+    private Long classId;
+    private String className;
 }
+
+    
+
