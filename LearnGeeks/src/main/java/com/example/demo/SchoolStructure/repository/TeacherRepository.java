b2e510322733d2a7bc556463a62cd71e01f4e1dd
Kayinamura-Karimba-Geofrey <geofreykayin@gmail.com>
2025-12-20 21:12:26 +0200

    first

--- LearnGeeks/src/main/java/com/example/demo/SchoolStructure/repository/TeacherRepository.java
+++ LearnGeeks/src/main/java/com/example/demo/SchoolStructure/repository/TeacherRepository.java
@@ -1,5 +1,15 @@
 package com.example.demo.SchoolStructure.repository;
 
-public class TeacherRepository {
-    
+import com.example.demo.SchoolStructure.Model.TeacherModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface TeacherRepository extends JpaRepository<TeacherModel, Long> {
+
+    Optional<TeacherModel> findByEmployeeNumber(String employeeNumber);
+
+    boolean existsByEmployeeNumber(String employeeNumber);
 }
