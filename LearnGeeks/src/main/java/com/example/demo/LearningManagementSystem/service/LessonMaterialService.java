2c3696818d07286d535135d398973c25470fa787
Kayinamura-Karimba-Geofrey <geofreykayin@gmail.com>
2025-12-21 22:48:20 +0200

    learning

--- LearnGeeks/src/main/java/com/example/demo/LearningManagementSystem/service/LessonMaterialService.java
+++ LearnGeeks/src/main/java/com/example/demo/LearningManagementSystem/service/LessonMaterialService.java
@@ -1,5 +1,13 @@
 package com.example.demo.LearningManagementSystem.service;
 
-public class LessonMaterialService {
-    
+import com.example.demo.LearningManagementSystem.DTO.CreateLessonMaterialRequest;
+import com.example.demo.LearningManagementSystem.DTO.LessonMaterialResponse;
+
+import java.util.List;
+
+public interface LessonMaterialService {
+
+    LessonMaterialResponse createLessonMaterial(CreateLessonMaterialRequest request);
+
+    List<LessonMaterialResponse> getMaterialsByModule(Long moduleId);
 }
--- LearnGeeks/src/main/java/com/example/demo/LearningManagementSystem/service/StudentCourseProgressService.java
+++
@@ -1,5 +0,0 @@
-package com.example.demo.LearningManagementSystem.service;
-
-public class StudentCourseProgressService {
-    
-}
