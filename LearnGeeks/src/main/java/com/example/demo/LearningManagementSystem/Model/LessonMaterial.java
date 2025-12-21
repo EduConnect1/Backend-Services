582937a38cfeb5ffb6cadd12d162fc3d0dfb474b
Kayinamura-Karimba-Geofrey <geofreykayin@gmail.com>
2025-12-21 17:20:25 +0200

    attendance

--- LearnGeeks/src/main/java/com/example/demo/LearningManagementSystem/Model/LessonMaterial.java
+++ LearnGeeks/src/main/java/com/example/demo/LearningManagementSystem/Model/LessonMaterial.java
@@ -1,5 +1,27 @@
 package com.example.demo.LearningManagementSystem.Model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "lesson_materials")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class LessonMaterial {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String contentUrl; // PDF/video link
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "module_id")
+    private Module module;
 }
