eda8eafbbfc070fea24e651c13da9b2f76863d05
Kayinamura-Karimba-Geofrey <geofreykayin@gmail.com>
2025-12-21 15:37:11 +0200

    intendance

---
+++ LearnGeeks/src/main/java/com/example/demo/Assignment/DTO/AssignmentResponse.java
@@ -0,0 +1,20 @@
+package com.example.demo.Assignment.DTO;
+
+
+
+import java.time.LocalDateTime;
+
+public record AssignmentResponse(
+
+        Long id,
+        String title,
+        String description,
+        Long subjectId,
+        String subjectName,
+        Long teacherId,
+        String teacherName,
+        LocalDateTime deadline,
+        LocalDateTime createdAt
+) {
+}
+
