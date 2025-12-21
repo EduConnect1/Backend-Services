e95f5ee344663f82f260faaa9dec4200c2593956
Kayinamura-Karimba-Geofrey <geofreykayin@gmail.com>
2025-12-21 22:41:40 +0200

    intendance

--- LearnGeeks/src/main/java/com/example/demo/LearningManagementSystem/service/ModuleService.java
+++ LearnGeeks/src/main/java/com/example/demo/LearningManagementSystem/service/ModuleService.java
@@ -1,5 +1,15 @@
 package com.example.demo.LearningManagementSystem.service;
 
-public class ModuleService {
-    
+
+
+import com.example.demo.LearningManagementSystem.DTO.CreateModuleRequest;
+import com.example.demo.LearningManagementSystem.DTO.ModuleResponse;
+
+import java.util.List;
+
+public interface ModuleService {
+
+    ModuleResponse createModule(CreateModuleRequest request);
+
+    List<ModuleResponse> getModulesByCourse(Long courseId);
 }
