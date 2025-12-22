ef2346bf09d9400a1b6b6f140502d3034305778c
Kayinamura-Karimba-Geofrey <geofreykayin@gmail.com>
2025-12-22 22:09:37 +0200

    card

--- LearnGeeks/src/main/java/com/example/demo/ResultManagement/repository/ReportCardRepository.java
+++ LearnGeeks/src/main/java/com/example/demo/ResultManagement/repository/ReportCardRepository.java
@@ -1,5 +1,7 @@
 package com.example.demo.ResultManagement.repository;
 
-public class ReportCardRepository {
-    
-}
+import com.example.demo.ResultManagement.Model.ReportCard;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReportCardRepository extends JpaRepository<ReportCard, Long> {}
+
