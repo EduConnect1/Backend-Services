ddaac6a80bce677f4534ac138d097ff63f10ed4c
Kayinamura-Karimba-Geofrey <geofreykayin@gmail.com>
2025-12-22 14:29:59 +0200

    exam

--- LearnGeeks/src/main/java/com/example/demo/OnlineExam/service/ExamAttemptService.java
+++ LearnGeeks/src/main/java/com/example/demo/OnlineExam/service/ExamAttemptService.java
@@ -1,5 +1,14 @@
 package com.example.demo.OnlineExam.service;
 
-public class ExamAttemptService {
-    
+import com.example.demo.OnlineExam.DTO.ExamResultResponse;
+import com.example.demo.OnlineExam.DTO.StartExamRequest;
+import com.example.demo.OnlineExam.DTO.SubmitAnswerRequest;
+
+public interface ExamAttemptService {
+
+    Long startExam(StartExamRequest request);
+
+    void submitAnswer(SubmitAnswerRequest request);
+
+    ExamResultResponse submitExam(Long attemptId);
 }
