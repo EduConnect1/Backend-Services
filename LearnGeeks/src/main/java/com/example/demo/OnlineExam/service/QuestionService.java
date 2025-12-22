a87a5ce76c8f0e4e2468819cf2ded6146f8dc3ac
Kayinamura-Karimba-Geofrey <geofreykayin@gmail.com>
2025-12-22 14:26:20 +0200

    exam

--- LearnGeeks/src/main/java/com/example/demo/OnlineExam/service/QuestionService.java
+++ LearnGeeks/src/main/java/com/example/demo/OnlineExam/service/QuestionService.java
@@ -1,5 +1,13 @@
 package com.example.demo.OnlineExam.service;
 
-public class QuestionService {
-    
+import com.example.demo.OnlineExam.DTO.CreateQuestionRequest;
+import com.example.demo.OnlineExam.DTO.QuestionResponse;
+
+import java.util.List;
+
+public interface QuestionService {
+
+    QuestionResponse createQuestion(CreateQuestionRequest request);
+
+    List<QuestionResponse> getQuestionsByExam(Long examId);
 }
