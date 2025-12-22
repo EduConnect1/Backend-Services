b49154902d20f566b73b580b1741c44bdd042468
Kayinamura-Karimba-Geofrey <geofreykayin@gmail.com>
2025-12-22 12:13:13 +0200

    intendance

--- LearnGeeks/src/main/java/com/example/demo/OnlineExam/Model/Option.java
+++ LearnGeeks/src/main/java/com/example/demo/OnlineExam/Model/Option.java
@@ -1,5 +1,26 @@
 package com.example.demo.OnlineExam.Model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "options")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Option {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "question_id")
+    private Question question;
+
+    private String optionText;
+
+    private boolean correct;
 }
---
+++ LearnGeeks/src/main/java/com/example/demo/OnlineExam/Model/StudentExamAttempt.java
@@ -0,0 +1,37 @@
+package com.example.demo.OnlineExam.Model;
+
+
+
+import com.example.demo.SchoolStructure.Model.Student;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "student_exam_attempts")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class StudentExamAttempt {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Student student;
+
+    @ManyToOne
+    private Exam exam;
+
+    private LocalDateTime startTime;
+
+    private LocalDateTime endTime;
+
+    private int score;
+
+    private boolean submitted;
+}
