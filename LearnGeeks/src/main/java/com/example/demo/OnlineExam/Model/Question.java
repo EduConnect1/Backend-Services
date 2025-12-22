45c5439eee1e1b98f7d33a65b99a5b823d4f26aa
Kayinamura-Karimba-Geofrey <geofreykayin@gmail.com>
2025-12-22 12:09:34 +0200

    learning

--- LearnGeeks/src/main/java/com/example/demo/OnlineExam/Model/Question.java
+++ LearnGeeks/src/main/java/com/example/demo/OnlineExam/Model/Question.java
@@ -1,5 +1,32 @@
 package com.example.demo.OnlineExam.Model;
 
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "questions")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Question {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "exam_id")
+    private Exam exam;
+
+    private String questionText;
+
+    @Enumerated(EnumType.STRING)
+    private QuestionType type;
+
+    private int marks;
 }
+
