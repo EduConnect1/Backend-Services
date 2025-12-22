4451c423f8940227b98fbc561f87431c82437501
Kayinamura-Karimba-Geofrey <geofreykayin@gmail.com>
2025-12-22 13:24:00 +0200

    exam

--- LearnGeeks/src/main/java/com/example/demo/OnlineExam/DTO/QuestionResponse.java
+++ LearnGeeks/src/main/java/com/example/demo/OnlineExam/DTO/QuestionResponse.java
@@ -1,5 +1,15 @@
 package com.example.demo.OnlineExam.DTO;
 
-public class QuestionResponse {
-    
+import com.example.demo.OnlineExam.Model.QuestionType;
+
+import java.util.List;
+
+public record QuestionResponse(
+
+        Long id,
+        String questionText,
+        QuestionType type,
+        int marks,
+        List<OptionResponse> options
+) {
 }
