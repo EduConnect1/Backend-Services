3bbeee3632e3a53cadf5fec2851aff6eaaf2549a
Kayinamura-Karimba-Geofrey <geofreykayin@gmail.com>
2025-12-22 22:05:48 +0200

    card

--- LearnGeeks/src/main/java/com/example/demo/ResultManagement/repository/SubjectRepository.java
+++ LearnGeeks/src/main/java/com/example/demo/ResultManagement/repository/SubjectRepository.java
@@ -1,5 +1,6 @@
 package com.example.demo.ResultManagement.repository;
+import com.example.demo.ResultManagement.Model.Subject;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public class SubjectRepository {
-    
-}
+public interface SubjectRepository extends JpaRepository<Subject, Long> {}
+
