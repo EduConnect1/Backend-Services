767ba8e9eebb100b548b540c49c615e899f55188
Kayinamura-Karimba-Geofrey <geofreykayin@gmail.com>
2025-12-22 12:36:50 +0200

    exam

--- LearnGeeks/src/main/java/com/example/demo/OnlineExam/repository/StudentExamAnswerRepository.java
+++ LearnGeeks/src/main/java/com/example/demo/OnlineExam/repository/StudentExamAnswerRepository.java
@@ -1,5 +1,12 @@
 package com.example.demo.OnlineExam.repository;
 
-public class StudentExamAnswerRepository {
-    
+import com.example.demo.OnlineExam.Model.StudentExamAnswer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface StudentExamAnswerRepository
+        extends JpaRepository<StudentExamAnswer, Long> {
+
+    List<StudentExamAnswer> findByAttemptId(Long attemptId);
 }
--- LearnGeeks/src/main/java/com/example/demo/OnlineExam/repository/StudentExamAttemptRepository.java
+++ LearnGeeks/src/main/java/com/example/demo/OnlineExam/repository/StudentExamAttemptRepository.java
@@ -1,5 +1,15 @@
 package com.example.demo.OnlineExam.repository;
 
-public class StudentExamAttemptRepository {
-    
+import com.example.demo.OnlineExam.Model.StudentExamAttempt;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface StudentExamAttemptRepository
+        extends JpaRepository<StudentExamAttempt, Long> {
+
+    Optional<StudentExamAttempt> findByStudentIdAndExamId(
+            Long studentId,
+            Long examId
+    );
 }
