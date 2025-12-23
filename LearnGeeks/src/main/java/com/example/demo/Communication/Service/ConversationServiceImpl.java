2e6b629055ab1cd94cff1a2a7c43b37bf8b7c082
Kayinamura-Karimba-Geofrey <geofreykayin@gmail.com>
2025-12-24 00:23:02 +0200

    communication

--- LearnGeeks/src/main/java/com/example/demo/Communication/Service/ConversationServiceImpl.java
+++ LearnGeeks/src/main/java/com/example/demo/Communication/Service/ConversationServiceImpl.java
@@ -1,5 +1,33 @@
 package com.example.demo.Communication.Service;
 
-public class ConversationServiceImpl {
-    
+import com.example.demo.Communication.Model.Conversation;
+import com.example.demo.Communication.Repository.ConversationRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ConversationServiceImpl implements ConversationService {
+
+    private final ConversationRepository conversationRepository;
+
+    @Override
+    public Conversation getOrCreateConversation(Long parentId, Long teacherId) {
+
+        return conversationRepository
+                .findByParentIdAndTeacherId(parentId, teacherId)
+                .orElseGet(() -> conversationRepository.save(
+                        Conversation.builder()
+                                .parentId(parentId)
+                                .teacherId(teacherId)
+                                .build()
+                ));
+    }
+
+    @Override
+    public Conversation getConversationById(Long conversationId) {
+        return conversationRepository.findById(conversationId)
+                .orElseThrow(() ->
+                        new RuntimeException("Conversation not found"));
+    }
 }
