ab02c5d322db0e1af086f539b87ee9630863f82c
Kayinamura-Karimba-Geofrey <geofreykayin@gmail.com>
2025-12-23 12:55:06 +0200

    card

--- LearnGeeks/src/main/java/com/example/demo/Fees/Repository/StudentFeeRepository.java
+++ LearnGeeks/src/main/java/com/example/demo/Fees/Repository/StudentFeeRepository.java
@@ -1,5 +1,11 @@
 package com.example.demo.Fees.Repository;
 
-public class StudentFeeRepository {
-    
+import com.example.demo.Fees.Model.StudentFee;
+import com.example.demo.SchoolStructure.Model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface StudentFeeRepository extends JpaRepository<StudentFee, Long> {
+    List<StudentFee> findByStudent(Student student);
 }
