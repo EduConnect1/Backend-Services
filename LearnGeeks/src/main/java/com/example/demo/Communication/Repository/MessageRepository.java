9261206bf4ce2c6078b751da81f51fdcc725dc5b
Kayinamura-Karimba-Geofrey <geofreykayin@gmail.com>
2025-12-23 23:25:14 +0200

    communication

--- LearnGeeks/src/main/java/com/example/demo/Communication/Repository/MessageRepository.java
+++ LearnGeeks/src/main/java/com/example/demo/Communication/Repository/MessageRepository.java
@@ -1,5 +1,14 @@
 package com.example.demo.Communication.Repository;
 
-public class MessageRepository {
-    
+import com.example.demo.Communication.Model.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface MessageRepository extends JpaRepository<Message, Long> {
+
+    List<Message> findByConversationIdOrderBySentAtAsc(Long conversationId);
+
+    long countByConversationIdAndReadFalse(Long conversationId);
 }
+
