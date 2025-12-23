f28d94b144a04819ac7073399ea6a41b21c91892
Kayinamura-Karimba-Geofrey <geofreykayin@gmail.com>
2025-12-23 23:29:20 +0200

    communication

--- LearnGeeks/src/main/java/com/example/demo/Communication/Repository/ConversationRepository.java
+++ LearnGeeks/src/main/java/com/example/demo/Communication/Repository/ConversationRepository.java
@@ -1,5 +1,16 @@
 package com.example.demo.Communication.Repository;
 
-public class ConversationRepository {
-    
+import com.example.demo.Communication.Model.Conversation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ConversationRepository extends JpaRepository<Conversation, Long> {
+
+    List<Conversation> findByParentId(Long parentId);
+
+    List<Conversation> findByTeacherId(Long teacherId);
+
+    Optional<Conversation> findByParentIdAndTeacherId(Long parentId, Long teacherId);
 }
