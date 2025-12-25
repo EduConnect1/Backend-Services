fcafa3b07e322e6ae8ed3f59340e08cd03f60ad2
Kayinamura-Karimba-Geofrey <geofreykayin@gmail.com>
2025-12-25 12:18:12 +0200

    analytics

--- LearnGeeks/src/main/java/com/example/demo/Analytics/DTO/PerformanceTrendResponse.java
+++ LearnGeeks/src/main/java/com/example/demo/Analytics/DTO/PerformanceTrendResponse.java
@@ -1,5 +1,9 @@
 package com.example.demo.Analytics.DTO;
 
-public class PerformanceTrendResponse {
-    
-}
+import java.time.LocalDate;
+
+public record PerformanceTrendResponse(
+
+        LocalDate date,
+        double averageScore
+) {}
